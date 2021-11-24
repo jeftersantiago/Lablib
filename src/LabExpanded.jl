037569f0e6d54e69a8f57d28c4887e8e54146b1d
eru-iluv <emc2rcf@gmail.com>
2021-11-24 17:47:09 -0300

    Testando

--- src/LabExpanded.jl
+++ src/LabExpanded.jl
@@ -1,7 +1,6 @@
 #;; Author: Edgard Macena Cabral
 #;; URL: https://github.com/eru-iluv
-#= LabExpanded é o módulo que inclui todas as funções ligadas a Measurements, mas que não estão diretamente relacionadas ao cálculo e
-propagação de incerteza.
+#= LabExpanded é o módulo que inclui todas as funções ligadas a Measurements, mas que não estão diretamente relacionadas ao cálculo e propagação de incerteza. O principal aqui é ser capaz de exportar tudo como latex table
 =#
 
 include("LabMeasurements.jl")
